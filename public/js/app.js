8413ac23f579d0b41edcaf36d8f44f7d37713733
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-05 11:54:29 -0400

    Added forn in frontedn to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,31 @@ socket.on('connect', function(){
 
     console.log('Connected to socket.io server');
 })
+
+socket.on('message', function(message){
+    console.log('New message: ');
+    console.log(message.text);
+})
+
+//Handles submitting of new message
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event){
+
+        // used to handle form submissison
+       event.preventDefault();
+ 
+        var $message = $form.find('input[name = message]')
+       
+       //sending message to server
+       socket.emit('message',{
+
+        text: $message.val()
+       });
+
+       $message.val(' ');
+
+       
+});
+
