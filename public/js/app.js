3b4c9c04ec01c80ee74074516778e10d852433ca
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-06 00:05:59 -0400

    Added support for chat room

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,43 @@
-var name = getQueryVariable('name') ||'Anonymous';
+var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
-
-
 var socket = io();
 
+console.log(name + ' wants to join ' + room);
 
+// Update h1 tag
+jQuery('.room-title').text(room);
 
-console.log(name + ' just joined ' + room);
-
-
-socket.on('connect', function(){
-
-    console.log('Connected to socket.io server');
-})
-
-socket.on('message', function(message){
-
-    var $message = jQuery('.messages');
-
-    var momentTimeStamp = moment.utc(message.timestamp);
-    console.log('New message: ');
-    console.log(message.text);
-
-    $message.append('<p><strong>'+ message.name+' '+momentTimeStamp.local().format('h:mm a') + '</strong></p>');
-    $message.append('<p>' + message.text+'</p>');
-})
-
-//Handles submitting of new message
-
-var $form = jQuery('#message-form');
-
-$form.on('submit', function(event){
-
-        // used to handle form submissison
-       event.preventDefault();
- 
-        var $message = $form.find('input[name = message]')
-       
-       //sending message to server
-       socket.emit('message',{
-        name: name,
-
-        text: $message.val()
-       });
-
-       $message.val(' ');
-
-       
+socket.on('connect', function () {
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
+socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
+
+	console.log('New message:');
+	console.log(message.text);
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
+});
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		name: name,
+		text: $message.val()
+	});
+
+	$message.val('');
+});
--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
-var http =require('http').Server(app);
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
-io.on('connection',function(socket){
 
-    console.log('User connected via socket.io');
+var clientInfo = {};
 
-    
-    socket.on('message', function(message){
+io.on('connection', function (socket) {
+	console.log('User connected via socket.io!');
 
-        console.log("Message received: " + message.text);
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req;
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		});
+	});
 
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
+		message.timestamp = moment().valueOf();
+		io.to(clientInfo[socket.id].room).emit('message', message);
+	});
 
-        /* //sending messages to everyone
-        socket.broadcast.emit('message', message); */
+	// timestamp property - JavaScript timestamp (milliseconds)
 
-        //SENDING TO EVERYONE INCLUDING THE SENDER
-        io.emit('message', message);
-
-    });
-
-    socket.emit('message',{
-
-        name:'System',
-
-        text: "Welcome to the chat application",
-        timestamp: moment().valueOf()
-    })
+	socket.emit('message', {
+		name: 'System',
+		text: 'Welcome to the chat application!',
+		timestamp: moment().valueOf()
+	});
 });
 
-http.listen(PORT, function(){
-
-    console.log('Server started');
-})
+http.listen(PORT, function () {
+	console.log('Server started!');
+});
